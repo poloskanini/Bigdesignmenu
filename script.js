4b7b94caf71b0b7cf4d52eee963a7a900a45d546
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2023-04-14 11:23:32 +0200

    Version 1.1 with style.css / script.js ISO .ts

---
+++ script.js
@@ -0,0 +1,37 @@
+const items = document.querySelectorAll('.item');
+
+const ratio = .9;
+
+const options = {
+  root: null,
+  // rootMargin: '-50px',
+  rootMargin: '-40px',
+  threshold: ratio
+}
+
+const handleIntersect = function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target);
+    }
+  })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.item').forEach(function(r) {
+  observer.observe(r);
+})
+
+// const observer = new IntersectionObserver((entries, observer) => {
+//   entries.forEach(entry => {
+//     if(entry.isIntersecting) {
+//       console.log(entry.target);
+//       entry.target.classList.add('visible');
+//     }
+//   })
+// })
+
+// items.forEach((item) => {
+//   observer.observe(item);
+// });
